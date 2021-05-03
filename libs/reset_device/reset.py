a5fcbaea0727b7411b439f9bede96fce3546e00f
Neil Merchant <phozee@gmail.com>
2021-05-03 02:43:10 -0600

    Set WiFi name to use deeplight id

--- libs/reset_device/reset.py
+++ libs/reset_device/reset.py
@@ -8,7 +8,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 counter = 0
-serial_last_four = subprocess.check_output(['cat', '/proc/cpuinfo'])[-5:-1].decode('utf-8')
+deeplight_id = subprocess.check_output(['cat', '/home/pi/deeplight.config'])[-5:-1].decode('utf-8')
 config_hash = reset_lib.config_file_hash()
 ssid_prefix = config_hash['ssid_prefix'] + " "
 reboot_required = False
@@ -16,7 +16,7 @@ reboot_required = False
 
 reboot_required = reset_lib.wpa_check_activate(config_hash['wpa_enabled'], config_hash['wpa_key'])
 
-reboot_required = reset_lib.update_ssid(ssid_prefix, serial_last_four)
+reboot_required = reset_lib.update_ssid(ssid_prefix, deeplight_id)
 
 if reboot_required == True:
     os.system('reboot')
